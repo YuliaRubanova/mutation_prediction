3a4efe260737e1fdf24e5681ec22f7000c0d84cf
YuliaRubanova <julia_erise@mail.ru>
2018-02-21 15:17:31 -0500

    Added different analysis of the data such as distributions of non-zero values

--- classify_mutation_real_fake.py
+++ classify_mutation_real_fake.py
@@ -20,6 +20,8 @@ import pandas as pd
 import glob
 from training_set import *
 from plot_signatures import *
+from scipy.stats.stats import pearsonr
+import sklearn
 
 from tensorflow.examples.tutorials.mnist import input_data
 
@@ -37,7 +39,9 @@ DEF_FEATURE_PATH = DIR + "dna_features_ryoga/"
 #dataset_path = "/Users/yulia/Documents/mutational_signatures/mutation_prediction_data/region_dataset.regionsize1000.small.pickle"
 #mut_dataset_path = [DIR + "/mutation_prediction_data/region_dataset.mutTumour10000.mutations_only.part*.pickle"]
 mut_dataset_path = [DIR + "/mutation_prediction_data/region_dataset.mutTumour10000.mutations_only.part*.pickle"]
-model_save_path = "trained_models/model.region_dataset.model{}.tumours{}.mut{}/model.train_test_vaf.ckpt"
+file_name = os.path.basename(__file__)[:-3]
+model_dir = "trained_models/model." + file_name + ".tumours{}.mut{}/"
+model_save_path = model_dir + "model.ckpt"
 dataset_with_annotation = DIR + "/mutation_prediction_data/region_dataset.mutTumour10000.region_size{region_size}.ID{{id}}.over_time.annotation.hdf5"
 
 #session_conf = tf.ConfigProto(gpu_options=gpu_options)
@@ -154,7 +158,7 @@ def make_model(x_dim, n_unique_tumours, z_latent_dim, model_type, model_save_pat
 def train(tf_vars, n_epochs, batch_size, model_save_path, available_tumours):
 	x, x_tumour_ids, y_, log_y_prediction = tf_vars
 
-	training_set, labels, n_unique_tumours, tumour_ids, mut_vaf, mut_annotation, feature_names = read_tumour_data(available_tumours)
+	training_set, labels, n_unique_tumours, tumour_ids, mut_vaf, mut_annotation, feature_names = read_tumour_data(available_tumours, region_size)
 	print("Optimizing...")
 	# config=tf.ConfigProto(gpu_options=gpu_options)
 	# config=session_conf
@@ -163,7 +167,7 @@ def train(tf_vars, n_epochs, batch_size, model_save_path, available_tumours):
 		for j in range(n_epochs):
 			for k in range(len(available_tumours) // n_tumours_per_batch+1):
 				if n_tumours_per_batch < len(available_tumours):
-					training_set, labels, n_unique_tumours, tumour_ids, mut_vaf, mut_annotation, feature_names = read_tumour_data(available_tumours, n_tumours_per_batch, k)
+					training_set, labels, n_unique_tumours, tumour_ids, mut_vaf, mut_annotation, feature_names = read_tumour_data(available_tumours, region_size, n_tumours_per_batch, k)
 					if training_set is None:
 						continue # no samples in a batch
 
@@ -202,9 +206,34 @@ def train(tf_vars, n_epochs, batch_size, model_save_path, available_tumours):
 		save_path = saver.save(sess, model_save_path)
 		print("Model saved in file: %s" % save_path)
 
+
+def non_zero(data):
+	return np.sum(data != 0) / np.prod(data.shape)
+
+def correlation_binary(x_input, y_input):
+	# see https://en.wikipedia.org/wiki/Phi_coefficient
+	x = np.copy(x_input)
+	y = np.copy(y_input)
+
+	x[x > 0] = 1
+	y[y > 0] = 1
+
+	n1d = np.sum(x == 1)
+	n0d = np.sum(x == 0)
+
+	nd1 = np.sum(y == 1)
+	nd0 = np.sum(y == 0)
+
+	n11 = np.sum((x == 1) & (y == 1))
+	n10 = np.sum((x == 1) & (y == 0))
+	n01 = np.sum((x == 0) & (y == 1))
+	n00 = np.sum((x == 0) & (y == 0))
+
+	phi = (n11 * n00 - n10 * n01) / np.sqrt(n1d * n0d * nd0 * nd1)
+	return phi
+
 def visualize(training_set, labels, n_unique_tumours, tumour_ids, mut_vaf, mut_annotation, feature_names, region_size):
 	# choose only true mutations
-
 	real_examples = np.where(labels.ravel() > 0)[0]
 	real_mutations = training_set[real_examples]
 	real_tumour_ids = tumour_ids[real_examples]
@@ -216,21 +245,145 @@ def visualize(training_set, labels, n_unique_tumours, tumour_ids, mut_vaf, mut_a
 
 	# plot distribution over features
 	real_mutations = np.reshape(real_mutations[:,96:], [real_mutations.shape[0], region_size, -1])
-	print(np.sum(real_mutations[:,:,:] > 0) / np.prod(real_mutations[:,:,:].shape))
+	prop_non_zero = non_zero(real_mutations)
+	print("Proportion of non-zero values: "+ str(prop_non_zero))
+
+	# identify useless features (with more zeros than others)
+	features = np.reshape(feature_names[96:], [region_size, -1])[50]
+	print("Non-zero features")
+
+	if (False):
+		print("Per position:")
+		for i in range(real_mutations.shape[1]):
+			print(str(i) + ": " + str(non_zero(real_mutations[:,i,:])))
+
+		print("\n\n")
+		print("Per feature:")
+		for i in range(real_mutations.shape[2]):
+			print(features[i])
+			print(non_zero(real_mutations[:,50,i]))
+
+	print(features)
+	print(np.apply_along_axis(lambda x: np.mean(x != 0), 0, real_mutations[:,50,:].astype(float)))
+
+	if (False):
+		print("\n\n")
+		print("Feature variability across positions:")
+		for i in range(real_mutations.shape[2]):
+			print(features[i])
+			print(np.std(real_mutations[:,:,i]))
+
+	print("\n\n")
+
+	if (False):
+		print("Correlation between type and features")
+		for i in range(mut_types.shape[1]):
+			print("mut_type " + feature_names[i])
+			# binary correlation and pearson correlation give the same result
+			#correlation = [pearsonr(mut_types[:,i], real_mutations[:,50,j])[0] for j in range(real_mutations.shape[2])]
+			correlation = [correlation_binary(mut_types[:,i], real_mutations[:,50,j]) for j in range(real_mutations.shape[2])]
+			maximum_val = np.argmax(np.array(np.abs(correlation)))
+			print("max correlation: " + str(correlation[maximum_val]) + " " + features[maximum_val])
+			#print(correlation)
+
+
+	print("# non-zero values per sample")
+	print(np.mean([len(real_mutations[i,50][real_mutations[i,50] != 0]) for i in range(real_mutations.shape[0])]))
+
 	# plot only features at the location of mutation (all feature types for all mutations)
+	# put the dimension over features first
 	non_zero_features = np.transpose(real_mutations[:,50,:]).tolist()
+
+	# plotting only non-zero values which make up a very small fraction of the data!
 	for i in range(len(non_zero_features)):
-		# transforming into log scale!!
-		non_zero_features[i] = [np.log(x) for x in non_zero_features[i] if x > 0]
-	plot_boxplot(non_zero_features, "region_features.pdf", np.reshape(feature_names[96:], [region_size, -1])[50])
+		non_zero_features[i] = [x for x in non_zero_features[i] if x != 0]
 
-	# compare distr over types in different tumours
+	plot_boxplot(non_zero_features, "region_features.pdf", features,
+		text_="Plot of non-zero features. Proportion of non-zero: " + str(round(prop_non_zero,6)))
 
-	# plot distrubtion over types over time
-
+	if (False):
 	# plot distribution over features for each type
+		percentage_non_zero_all_types = []
+		type_names = []
+		for mtype in range(6):
+			mtype_name = feature_names[mtype*16][:3]
+			type_names.append(mtype_name)
+			print(mtype_name)
 
-	# plot distribution over features for each tumour
+			mut_types_current = np.sum(mut_types[:,mtype*16:(mtype+1)*16], axis=1)
+			mut_with_type = real_mutations[np.where(mut_types_current == 1)[0]]
+			print(mut_with_type.shape)
+
+			prop_non_zero_type = non_zero(mut_with_type)
+			print("Proportion of non-zero: " + str(prop_non_zero_type))
+
+			mut_with_type = np.transpose(mut_with_type[:,50,:])
+
+			# plotting only non-zero values which make up a very small fraction of the data!
+			non_zero_features_type = mut_with_type.tolist()
+			for i in range(len(non_zero_features_type)):
+				non_zero_features_type[i] = [x for x in non_zero_features_type[i] if x != 0]
+
+			plot_boxplot(non_zero_features_type, "region_features" + mtype_name + ".pdf", features,
+				text_="Plot of non-zero features. Proportion of non-zero: " + str(round(prop_non_zero_type,6)))
+
+			plot_bars([non_zero(x) for x in mut_with_type], "percentage_non_zero" + mtype_name + ".pdf", features, 
+				ylabel = "Percentage of non-zero values", ylim=[0, 0.2],
+				text_="Type " + mtype_name + ". # mutations: " + str(int(mut_with_type.shape[1])))
+
+			correlation = [correlation_binary(mut_types_current, real_mutations[:,50,j]) for j in range(real_mutations.shape[2])]
+			maximum_val = np.argmax(np.array(np.abs(correlation)))
+			print("max correlation: " + str(correlation[maximum_val]) + " " + features[maximum_val])
+
+			non_zero_first_feature = (real_mutations[:,50,0] > 0).astype(float)
+			random_types = np.random.multinomial(np.sum(mut_types_current), [1/mut_types_current.shape[0]]*mut_types_current.shape[0] )
+			random_features = np.random.multinomial(np.sum(non_zero_first_feature), [1/non_zero_first_feature.shape[0]]*non_zero_first_feature.shape[0] )
+			print("Correlation by random: " + str(correlation_binary(random_types, random_features)))
+
+			percentage_non_zero_all_types.append([non_zero(x) for x in mut_with_type])
+
+	if (False):
+		# compare distr over types in different tumours
+		for id_ in np.unique(real_tumour_ids):
+			print("Tumour " + str(id_))
+
+			mut_tumour = real_mutations[np.where(real_tumour_ids == id_)[0]]
+
+			prop_non_zero_type = non_zero(mut_tumour)
+			print("Proportion of non-zero: " + str(prop_non_zero_type))
+
+			mut_tumour = np.transpose(mut_tumour[:,50,:])
+
+			plot_bars([non_zero(x) for x in mut_tumour], "percentage_non_zero.tumour" + str(id_) + ".pdf", features, 
+				ylabel = "Percentage of non-zero values", ylim=[0, 0.4],
+				text_="Tumour " + str(id_) + ". # mutations: " + str(int(mut_tumour.shape[1])))
+
+	if False:
+		# Plot number of non-zero values for different VAFs
+		print("VAF range " + "> 1")
+		mut_vaf = real_mutations[np.where(real_mut_vaf > 1)[0]]
+		prop_non_zero_vaf = non_zero(mut_vaf)
+		print("Proportion of non-zero: " + str(prop_non_zero_vaf))
+
+		mut_vaf = np.transpose(mut_vaf[:,50,:])
+
+		plot_bars([non_zero(x) for x in mut_vaf], "percentage_non_zero.vaf>1.pdf", features, 
+			ylabel = "Percentage of non-zero values", ylim=[0, 0.2],
+			text_="VAF > 1. # mutations: " + str(int(mut_vaf.shape[1])))
+
+		for vaf_range in np.arange(0.9,0,-0.1):
+			vaf_range = round(vaf_range, 2)
+			range_str = "[" + str(vaf_range) + "," + str(round(vaf_range+0.1,2)) + "]"
+			print("VAF range " + range_str)
+			mut_vaf = real_mutations[np.where((real_mut_vaf > vaf_range) & (real_mut_vaf < (vaf_range+0.1)))[0]]
+			prop_non_zero_vaf = non_zero(mut_vaf)
+			print("Proportion of non-zero: " + str(prop_non_zero_vaf))
+
+			mut_vaf = np.transpose(mut_vaf[:,50,:])
+
+			plot_bars([non_zero(x) for x in mut_vaf], "percentage_non_zero.vaf" + range_str + ".pdf", 
+				features, ylabel = "Percentage of non-zero values", ylim=[0, 0.25],
+				text_="VAF " + range_str + ". # mutations: " + str(int(mut_vaf.shape[1])))
 
 
 if __name__ == '__main__':
@@ -297,7 +450,7 @@ if __name__ == '__main__':
 	print("Processing {} mutations from {} tumour(s) ...".format(n_mut, n_unique_tumours))
 
 	tf.reset_default_graph()
-	model_save_path = model_save_path.format(model_type, n_tumours, n_mut)
+	model_save_path = model_save_path.format(n_tumours, n_mut)
 	os.makedirs(model_save_path, exist_ok=True)
 
 	tf_vars, metrics, meta, extra = make_model(num_features, n_unique_tumours, z_latent_dim, model_type, model_save_path, adam_rate = adam_rate)
@@ -321,9 +474,17 @@ if __name__ == '__main__':
 		with tf.Session() as sess:
 			saver.restore(sess, model_save_path)
 
-			training_set, labels, n_unique_tumours, tumour_ids, mut_vaf, mut_annotation, feature_names = read_tumour_data(available_tumours)
+			training_set, labels, n_unique_tumours, tumour_ids, mut_vaf, mut_annotation, feature_names = read_tumour_data(available_tumours, region_size)
 			x_train, x_test, y_train, y_test, x_tumour_ids_train, x_tumour_ids_test = train_test_split([training_set, labels, tumour_ids], split_by = mut_vaf, test_size=0.2)
 			test_dict = {x: x_test, y_: y_test, x_tumour_ids: x_tumour_ids_test}
+
+			real_data = training_set[np.where(labels.ravel() > 0)[0]]
+			real_data = np.reshape(real_data[:,96:], [real_data.shape[0], region_size, -1])
+
+			print("Total real mutations: " + str(real_data.shape[0]))
+			print("Open chromatin: " + str(round(np.mean(real_data[:,50,0]),6)))
+			print("Transcribed: " + str(round(np.mean(real_data[:,50,1]),6)))
+			print("Strand: " + str(round(np.mean(real_data[:,50,2]),6))) 
 
 			visualize(training_set, labels, n_unique_tumours, tumour_ids, mut_vaf, mut_annotation, feature_names, region_size)
 
@@ -342,7 +503,7 @@ if __name__ == '__main__':
 			print("0: " + str(np.mean(correct_prediction[np.logical_not(y_test.ravel().astype(int))])))
 
 			print("Tumour representation std")
-			print(np.mean(np.std(z_t.eval(feed_dict=test_dict), axis=0)))
+			#print(np.mean(np.std(z_t.eval(feed_dict=test_dict), axis=0)))
 
 			if model_type == "gaussian":
 				print("Likelihood of region representation:")
